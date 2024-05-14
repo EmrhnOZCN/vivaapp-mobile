cfcaad3f74d3b138992fae9232a9d001d565bf29
EmrhnOZCN <emrhnozcnceng@gmail.com>
2024-05-14 17:04:00 +0300

    Codes for going to the login page and returning to the home page were written.

--- app/src/main/java/com/example/vivaapp_mobile/ui/LoginActivity.java
+++ app/src/main/java/com/example/vivaapp_mobile/ui/LoginActivity.java
@@ -1,26 +1,32 @@
 package com.example.vivaapp_mobile.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
 
+import com.example.vivaapp_mobile.MainActivity;
 import com.example.vivaapp_mobile.R;
+import com.example.vivaapp_mobile.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Inflate the layout using data binding
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // Set OnClickListener to the "Ana Sayfa" button using data binding
+        binding.anasayfaDon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch MainActivity when the button is clicked
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/vivaapp_mobile/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/vivaapp_mobile/ui/home/HomeFragment.java
@@ -1,30 +1,42 @@
 package com.example.vivaapp_mobile.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.vivaapp_mobile.ui.LoginActivity;
+
 import com.example.vivaapp_mobile.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+    private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
+        homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
+        // Set click listener for girisButon
+        binding.girisButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch LoginActivity when the button is clicked
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return root;
     }
